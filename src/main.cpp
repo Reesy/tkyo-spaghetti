756eaca86b272411fb2c65fb47daf15c61f6471b
James Rees <James.rees@outlook.com>
2020-02-12 02:25:52 +0900

    Cleaned up some of the function names and stopped rendering the bounding boxes

--- src/main.cpp
+++ src/main.cpp
@@ -5,61 +5,37 @@
 #include <math.h>
 
 //game objects
-sf::RenderWindow window(sf::VideoMode(430, 750), "Tkyo Spaghetti");
+sf::RenderWindow window(sf::VideoMode(1280, 720), "Tkyo Spaghetti");
 
 
 sf::Image icon;
-// Load a sprite to display
-sf::Texture texture;
+// Load a background to display
+sf::Texture background_texture;
 
 //pipe textures
 sf::Texture pipe_texture;
 sf::Texture paused_icon;
 
 sf::Sprite paused_sprite;
-sf::Sprite sprite;
-sf::Sprite pipe_sprite;
+sf::Sprite background;
+sf::Sprite floor_sprite;
 sf::Music music;
-
-
-sf::Texture top_pipe;
-sf::Texture bottom_pipe;
-
-sf::Sprite top_pipe_sprite;
-sf::Sprite bottom_pipe_sprite;
-
-sf::Sprite numbers;
 
 sf::RectangleShape birdBox(sf::Vector2f(55, 45));
 
-sf::RectangleShape topPipeBox(sf::Vector2f(70, 400));
-
-sf::RectangleShape bottomPipeBox(sf::Vector2f(70, 400));
+sf::RectangleShape floor_box(sf::Vector2f(1280, 50));
 
 
 sf::Font font;
 sf::Text text("So beta, wow", font, 50);
 
 sf::Texture birdTexture1;
-sf::Sprite birdSprite;
+sf::Sprite bird_sprite;
 
 sf::Texture birdTexture2;
 
 sf::Texture birdTexture3;
-
-sf::Texture One;
-sf::Texture Two;
-sf::Texture Three;
-sf::Texture Four;
-sf::Texture Five;
-sf::Texture Six;
-sf::Texture Seven;
-sf::Texture Eight;
-sf::Texture Nine;
-
-
 sf::Event event;
-std::vector<sf::Sprite> birdAnimation;
 
 int gap = 140; //change this to increase difficulty by making gaps from each pillar wider
 int pipe_gap; // This is the vertical gap between the pair of piped
@@ -71,18 +47,17 @@ float accumulator;
 float animateSpeed;
 bool started;
 bool countBlock;
-bool paused;
+bool collided;
 bool endState;
 bool flight; // this is true when the user has input a jump command for a specific unit of time.
 bool flapping;
 float timeOfClick;
 
-static void collided()
+static void checkCollision()
 { 
-    if(birdBox.getGlobalBounds().intersects(topPipeBox.getGlobalBounds()) || 
-       birdBox.getGlobalBounds().intersects(bottomPipeBox.getGlobalBounds()))
+    if (birdBox.getGlobalBounds().intersects(floor_box.getGlobalBounds()))
     {
-        paused = true;
+        collided = true;
     }
 }
 
@@ -90,58 +65,13 @@ static void update(float elapsed)
 {
     float delta = elapsed * 60;
     int speed = 5;
-    
-    pipe_sprite.move((-speed * delta), 0);
-    bottom_pipe_sprite.move((-speed * delta), 0);
-    bottomPipeBox.move((-speed * delta), 0);
-    topPipeBox.move((-speed * delta), 0);
-    
-    top_pipe_sprite.move((-speed * delta), 0);
+     
+    checkCollision();
 
-    if(pipe_sprite.getPosition().x < -40)
-    {
-        pipe_sprite.move(40, 0);
-    }
-  
-    if(bottomPipeBox.getPosition().x < 110 && countBlock == false)
-    {
-        score++;
-        countBlock = true;
-    }
- 
-    if(bottom_pipe_sprite.getPosition().x < -50)
-    {
-        int random_dist = rand() % 2;
-        
-        if (random_dist == 0 & bottom_pipe_sprite.getPosition().y < gap)
-        {
-            currentdistance += gap;
-        }
-        else
-        {
-            currentdistance -= gap;
-        }
-
-        top_pipe_sprite.move(500, 0);
-        bottom_pipe_sprite.move(500, 0);
-        bottomPipeBox.move(500, 0);
-        topPipeBox.move(500, 0);
-        
-        bottom_pipe_sprite.setPosition(bottom_pipe_sprite.getPosition().x, currentdistance);
-        bottomPipeBox.setPosition(bottomPipeBox.getPosition().x, currentdistance);
-        top_pipe_sprite.setPosition(top_pipe_sprite.getPosition().x, (400 + gap)+ currentdistance);
-        topPipeBox.setPosition(top_pipe_sprite.getPosition().x, (400 + gap)+ currentdistance);
-        countBlock = false;
-       
-    }
-    collided();
-    //gravity
-    
-    if(flight)
+    if (flight)
     {
         timeOfClick += delta;
-        std::cout << timeOfClick << std::endl;
-        if(timeOfClick > 15)
+        if (timeOfClick > 15)
         {
             flight = false;
         }
@@ -149,64 +79,44 @@ static void update(float elapsed)
     
     if (flight)
     {
-        birdSprite.move(0, -4);
+        bird_sprite.move(0, -4);
         birdBox.move(0, -4);
     } 
     else
     {
-        birdSprite.move(0, 4);
+        bird_sprite.move(0, 4);
         birdBox.move(0, 4);
     }
 }
 
 
 static void birdAnimate(float elapsedTime){
-    birdSprite.setTexture(birdTexture1);
+    bird_sprite.setTexture(birdTexture1);
     accumulator += elapsedTime * animateSpeed;
   
     if (accumulator > 1 && accumulator < 2)
     {
-        birdSprite.setTexture(birdTexture1);
+        bird_sprite.setTexture(birdTexture1);
     }
     else if (accumulator > 2 && accumulator < 3)
     {
-        birdSprite.setTexture(birdTexture2);
+        bird_sprite.setTexture(birdTexture2);
     }
     else if (accumulator > 3 && accumulator < 4)
     {
-        birdSprite.setTexture(birdTexture3);
+        bird_sprite.setTexture(birdTexture3);
     }
     else if (accumulator > 4)
     {
-        birdSprite.setTexture(birdTexture2);
+        bird_sprite.setTexture(birdTexture2);
         accumulator = 0;
     }
 }
 
 static void render()
 {
-    // Draw the sprite
-    window.draw(sprite);
-    
-    window.draw(bottom_pipe_sprite);
-    
-    window.draw(top_pipe_sprite);
-    
-    window.draw(pipe_sprite);
-    
-    window.draw(birdBox);
-   
-    window.draw(birdSprite);
-    
-    window.draw(bottomPipeBox);
-    
-    window.draw(topPipeBox);
-   
-    if (paused == true)
-    {
-        window.draw(paused_sprite);
-    }
-
+    window.draw(floor_sprite);
+    window.draw(bird_sprite);
 }
 
 static void input()
@@ -225,24 +135,24 @@ static void input()
     }
     
     // plays music
-    if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
     {
         timeOfClick = 0;
         flight = true;
     }
 
-    if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::K)
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::K)
     {
-        paused = false;
+        collided = false;
     }
-    if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Left)
     {
-        birdSprite.setTexture(birdTexture1);
+        bird_sprite.setTexture(birdTexture1);
     }
 
-    if(event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
+    if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Right)
     {
-        birdSprite.setTexture(birdTexture2);
+        bird_sprite.setTexture(birdTexture2);
     }
 }
 
@@ -250,10 +160,8 @@ static void loadResources()
 {
     // Set the Icon
 	icon.loadFromFile("resources/icon.png");
-    texture.loadFromFile("resources/flappy2.png", sf::IntRect(0, 0, 145, 250));
+    background_texture.loadFromFile("resources/flappy2.png", sf::IntRect(0, 0, 145, 250));
     pipe_texture.loadFromFile("resources/flappy2.png", sf::IntRect(150, 0, 150, 50));
-	bottom_pipe.loadFromFile("resources/flappy.png", sf::IntRect(302, 0, 27, 135));
-	top_pipe.loadFromFile("resources/flappy.png", sf::IntRect(330, 0, 27, 135));
 	paused_icon.loadFromFile("resources/flappy.png", sf::IntRect(242, 197, 40, 13));
 	birdTexture1.loadFromFile("resources/flappy.png", sf::IntRect(220, 120, 20, 20));
 	birdTexture2.loadFromFile("resources/flappy.png", sf::IntRect(261, 86, 20, 20));
@@ -264,40 +172,27 @@ static void loadResources()
 
 static void init()
 {
-    
+
     text.setColor(sf::Color::Black);
     window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
     window.setFramerateLimit(60);
-    sprite.setScale(3, 3);
-    sprite.setTexture(texture);
-    
-    pipe_sprite.setScale(4, 3);
-    pipe_sprite.setTexture(pipe_texture);
+    background.setScale(3, 3);
+    background.setTexture(background_texture);
+    floor_sprite.setScale(10, 3);
+    floor_sprite.setTexture(pipe_texture);
     paused_sprite.setTexture(paused_icon);
     paused_sprite.move(130, 200);
     paused_sprite.setScale(4,4);
     pipe_gap = 20;
-    bottom_pipe_sprite.setTexture(bottom_pipe);
-    bottom_pipe_sprite.setScale(3,3);
-    bottomPipeBox.move(600, 0);
-    
-    bottom_pipe_sprite.move(600, 0);
-    
-    top_pipe_sprite.setTexture(top_pipe);
-    top_pipe_sprite.setScale(3, 3);
-    top_pipe_sprite.move(600, 400 + gap);
-    topPipeBox.move(600, 400 + gap);
-    
-    birdSprite.setTexture(birdTexture1);
-    birdSprite.setScale(4, 4);
-    birdSprite.move(160, 240);
-    
+    floor_box.move(0, 600);
+    bird_sprite.setTexture(birdTexture1);
+    bird_sprite.setScale(4, 4);
+    bird_sprite.move(160, 240);
     birdBox.move(178,258);
-    
-    pipe_sprite.move(0, 600);
+    floor_sprite.move(0, 600);
     music.play();
     currentdistance = 0;
-    paused = false;
+    collided = false;
     started = true;
     endState = false;
     frameCounter = 0;
@@ -333,7 +228,7 @@ int main(int, char const**)
     
         birdAnimate(elapsed.asSeconds());
         render();
-        if (paused == false && started == true)
+        if (collided == false && started == true)
         {
             update(elapsed.asSeconds());
         }
